6670272b64b1c83c47d9b343f2635517e95f2a73
Viktor Ivanov <sessgreem@gmail.com>
2018-07-15 18:20:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,16 +12,17 @@ const codes = [
 ];
 
 function init() {
-  let idx = 0
+let index = 0;
+document.body.addEventListener('keydown',(e) => { 
+  const key = e.key;
+  
+  index = (codes[index]===key) ? ++index : 0;
+  
+  if (index === codes.length){
+    window.alert("Hurray!");
+  index  = 0;
+  }
 
-  document.body.addEventListener("keydown", (e) => {
-    const key = e.key
-
-    idx = (codes[idx] === key) ? ++idx : 0
-
-    if (idx === codes.length) {
-      window.alert("Hurray!");
-      idx = 0
-    }
-  });
+  
+});
 }
